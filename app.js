ef4c8d130a65a16a2f9f7ff216b5067d0f6d0012
Siba Prasad Prusty. <prustysibaprasad02@gmail.com>
2026-04-23 14:39:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write("🚀 Node.js DevOps Project Running");
+  res.write(" Node.js DevOps Project Running");
   res.end();
 });
 
