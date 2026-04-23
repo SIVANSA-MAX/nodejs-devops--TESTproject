594bd53e7a607e2a231e6b6f410b24a98b4274cf
Siba Prasad Prusty. <prustysibaprasad02@gmail.com>
2026-04-23 14:24:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write(" Node.js DevOps Project Running");
+  res.write("🚀 Node.js DevOps Project Running");
   res.end();
 });
 
@@ -9,6 +9,4 @@ const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-  console.log("Server running on port 3000");
 });
